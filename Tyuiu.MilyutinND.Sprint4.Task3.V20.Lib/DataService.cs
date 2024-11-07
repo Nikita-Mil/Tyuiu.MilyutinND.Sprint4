5124f6aacd14c2b9fff46b1a0d1fa56f641cbf5c
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-11-07 23:31:22 +0500

    Task3

--- Tyuiu.MilyutinND.Sprint4.Task3.V20.Lib/DataService.cs
+++ Tyuiu.MilyutinND.Sprint4.Task3.V20.Lib/DataService.cs
@@ -6,7 +6,33 @@ namespace Tyuiu.MilyutinND.Sprint4.Task3.V20.Lib
     {
         public int Calculate(int[,] array)
         {
-            
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length/rows; 
+
+            int count = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j == 0)
+                    {
+                        count = Math.Min(count, array[i, j]);
+                    }
+                }
+            }
+            return count;
         }
     }
 }
+//Дан двумерный целочисленный массив 5 на 5 элементов, заполненный
+//статическими значениями в диапазоне от 3 до 8. Найдите минимальный элемент в первом столбце массива.
+//8, 7, 7, 8, 5,
+
+//4, 3, 5, 3, 6,
+
+//5, 3, 8, 6, 3,
+
+//6, 3, 8, 5, 4,
+
+//3, 6, 8, 3, 4,
--- Tyuiu.MilyutinND.Sprint4.Task3.V20/Program.cs
+++ Tyuiu.MilyutinND.Sprint4.Task3.V20/Program.cs
@@ -1,1 +1,42 @@
 ﻿using Tyuiu.MilyutinND.Sprint4.Task3.V20.Lib;
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #20                                                             *");
+Console.WriteLine("* Выполнил: Милютин Никита Дмитриевич | СМАРТб-24-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* статическими значениями в диапазоне от 3 до 8. Найдите минимальный      *");
+Console.WriteLine("* элемент в первом столбце массива.                                       *");
+
+DataService ds = new DataService();
+int[,] array = new int[5, 5] { { 8, 7, 7, 8, 5 },
+                                           { 4, 3, 5, 3, 6 },
+                                           { 5, 3, 8, 6, 3 },
+                                           { 6, 3, 8, 5, 4 },
+                                           { 3, 6, 8, 3, 4 } };
+int rows = array.GetUpperBound(0) + 1;
+int columns = array.Length / rows;
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"{array[i, j]} \t");
+    }
+}
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine($"Минимальный элемент в первом столбце массива: {ds.Calculate(array)}");
+Console.ReadKey();
